269747484eef1f4e062b7b77a0f1677cd89764ed
egor-achkasov <eaachkasov@edu.hse.ru>
2024-09-11 15:18:16 +0200

    Rewrite rt/scene_object.py

--- sionna/rt/scene_object.py
+++ sionna/rt/scene_object.py
@@ -5,14 +5,18 @@
 """
 Class representing objects in the scene
 """
-import tensorflow as tf
+import numpy as np
 
 from .object import Object
 from .radio_material import RadioMaterial
 import drjit as dr
 import mitsuba as mi
-from .utils import mi_to_tf_tensor, angles_to_mitsuba_rotation, normalize,\
-    theta_phi_from_unit_vec
+from .utils import (
+    mi_to_np_ndarray,
+    angles_to_mitsuba_rotation,
+    normalize,
+    theta_phi_from_unit_vec,
+)
 
 
 class SceneObject(Object):
@@ -23,22 +27,22 @@ class SceneObject(Object):
     Every object in the scene is implemented by an instance of this class
     """
 
-    def __init__(self,
-                 name,
-                 object_id=None,
-                 mi_shape=None,
-                 radio_material=None,
-                 orientation=(0,0,0),
-                 dtype=tf.complex64,
-                 **kwargs):
+    def __init__(
+        self,
+        name,
+        object_id=None,
+        mi_shape=None,
+        radio_material=None,
+        orientation=(0, 0, 0),
+        dtype=np.complex_,
+        **kwargs,
+    ):
 
-        if dtype not in (tf.complex64, tf.complex128):
-            raise ValueError("`dtype` must be tf.complex64 or tf.complex128`")
         self._dtype = dtype
         self._rdtype = dtype.real_dtype
 
         # Orientation of the object is initialized to (0,0,0)
-        self._orientation = tf.cast(orientation, dtype=self._rdtype)
+        self._orientation = np.asarray(orientation, dtype=self._rdtype)
 
         # Initialize the base class Object
         super().__init__(name, **kwargs)
@@ -53,9 +57,9 @@ class SceneObject(Object):
         self._mi_shape = mi_shape
 
         # Set velocity vector
-        self.velocity = tf.cast([0,0,0], dtype=self._rdtype)
+        self.velocity = np.asarray([0., 0., 0.], dtype=self._rdtype)
 
-        if self._dtype == tf.complex64:
+        if self._dtype == np.complex_:
             self._mi_point_t = mi.Point3f
             self._mi_vec_t = mi.Vector3f
             self._mi_scalar_t = mi.Float
@@ -91,7 +95,7 @@ class SceneObject(Object):
 
     @radio_material.setter
     def radio_material(self, mat):
-        # Note: _radio_material is set at __init__, but pylint doesn't see it.
+        # Note: _radio_material is set at __init__, but pylint doesn't see it.
         if mat is None:
             mat_obj = None
 
@@ -102,8 +106,10 @@ class SceneObject(Object):
                 raise TypeError(err_msg)
 
         elif not isinstance(mat, RadioMaterial):
-            err_msg = ("The material must be a material name (str) or an "
-                        "instance of RadioMaterial")
+            err_msg = (
+                "The material must be a material name (str) or an "
+                "instance of RadioMaterial"
+            )
             raise TypeError(err_msg)
 
         else:
@@ -111,7 +117,7 @@ class SceneObject(Object):
 
         # Remove the object from the set of the currently used material, if any
         # pylint: disable=access-member-before-definition
-        if hasattr(self, '_radio_material') and self._radio_material:
+        if hasattr(self, "_radio_material") and self._radio_material:
             self._radio_material.discard_object_using(self.object_id)
         # Assign the new material
         # pylint: disable=access-member-before-definition
@@ -138,9 +144,9 @@ class SceneObject(Object):
 
     @velocity.setter
     def velocity(self, v):
-        if not tf.shape(v)==3:
+        if not np.shape(v) == 3:
             raise ValueError("`velocity` must have shape [3]")
-        self._velocity = tf.cast(v, self._rdtype)
+        self._velocity = np.asarray(v, self._rdtype)
 
     @property
     def position(self):
@@ -153,46 +159,45 @@ class SceneObject(Object):
         rdtype = self._scene.dtype.real_dtype
         # Bounding box
         # [3]
-        bbox_min = tf.cast(self._mi_shape.bbox().min, rdtype)
+        bbox_min = np.asarray(self._mi_shape.bbox().min, rdtype)
         # [3]
-        bbox_max = tf.cast(self._mi_shape.bbox().max, rdtype)
+        bbox_max = np.asarray(self._mi_shape.bbox().max, rdtype)
         # [3]
-        half = tf.cast(0.5, self._rdtype)
-        position = half*(bbox_min + bbox_max)
+        half = float(0.5)
+        position = half * (bbox_min + bbox_max)
         return position
 
     @position.setter
     def position(self, new_position):
 
-        ## Update Mitsuba vertices
+        # Update Mitsuba vertices
 
         # Scene parameters
         scene_params = self._scene.mi_scene_params
         # Real dtype
         rdtype = self._scene.dtype.real_dtype
-        new_position = tf.cast(new_position, rdtype)
+        new_position = np.asarray(new_position, rdtype)
         # [num_vertices*3]
-        vertices = scene_params[f'mesh-{self.name}.vertex_positions']
+        vertices = scene_params[f"mesh-{self.name}.vertex_positions"]
         # [num_vertices,3]
-        vertices = mi_to_tf_tensor(vertices, rdtype)
-        vertices = tf.reshape(vertices, [-1, 3])
+        vertices = mi_to_np_ndarray(vertices, rdtype)
+        vertices = np.reshape(vertices, [-1, 3])
         # [3]
         position = self.position
         # [3]
         translation_vector = new_position - position
         # [1,3]
-        translation_vector = tf.expand_dims(translation_vector, axis=0)
+        translation_vector = np.expand_dims(translation_vector, axis=0)
         # [num_vertices,3]
         translated_vertices = vertices + translation_vector
         # Cast to Mitsuba type to object the Mitsuba scene
-        fltn_translated_vertices = tf.reshape(translated_vertices, [-1])
+        fltn_translated_vertices = np.reshape(translated_vertices, [-1])
         fltn_translated_vertices = self._mi_scalar_t(fltn_translated_vertices)
         #
-        scene_params[f'mesh-{self.name}.vertex_positions'] =\
-            fltn_translated_vertices
+        scene_params[f"mesh-{self.name}.vertex_positions"] = fltn_translated_vertices
         scene_params.update()
 
-        ## Update Sionna vertices
+        # Update Sionna vertices
 
         obj_id = self.object_id
         mi_shape = self._mi_shape
@@ -201,8 +206,9 @@ class SceneObject(Object):
         shape_ind = solver_paths.shape_indices[obj_id]
         prim_offset = solver_paths.prim_offsets[shape_ind]
 
-        face_indices3 = mi_shape.face_indices(dr.arange(mi.UInt32,
-                                                        mi_shape.face_count()))
+        face_indices3 = mi_shape.face_indices(
+            dr.arange(mi.UInt32, mi_shape.face_count())
+        )
         # Flatten. This is required for calling vertex_position
         # [n_prims*3]
         face_indices = dr.ravel(face_indices3)
@@ -211,35 +217,34 @@ class SceneObject(Object):
         vertex_coords = mi_shape.vertex_position(face_indices)
         # Cast to TensorFlow type
         # [n_prims*3, 3]
-        vertex_coords = mi_to_tf_tensor(vertex_coords, rdtype)
+        vertex_coords = mi_to_np_ndarray(vertex_coords, rdtype)
         # Unflatten
         # [n_prims, vertices per triangle : 3, 3]
-        vertex_coords = tf.reshape(vertex_coords, [mi_shape.face_count(), 3, 3])
+        vertex_coords = np.reshape(vertex_coords, [mi_shape.face_count(), 3, 3])
         # Update the tensor storing the primitive vertices
-        sl = tf.range(prim_offset, prim_offset + mi_shape.face_count(),
-                    dtype=tf.int32)
-        sl = tf.expand_dims(sl, axis=1)
+        sl = np.arange(prim_offset, prim_offset + mi_shape.face_count(), dtype=np.int_)
+        sl = np.expand_dims(sl, axis=1)
         solver_paths.primitives.scatter_nd_update(sl, vertex_coords)
 
-        ## Update Sionna wedges
+        # Update Sionna wedges
 
         wedges_objects = solver_paths.wedges_objects
         wedges_origin = solver_paths.wedges_origin
 
         # Indices of the wedges corresponding to this object
         # [num_wedges]
-        wedges_ind, _ = tf.unique(tf.where(wedges_objects == obj_id)[:,0])
+        wedges_ind, _ = np.unique(np.where(wedges_objects == obj_id)[:, 0])
 
         # Corresponding origins
         # [num_wedges, 3]
-        wedges_origin = tf.gather(wedges_origin, wedges_ind, axis=0)
+        wedges_origin = np.take_along_axis(wedges_origin, wedges_ind, axis=0)
 
         # Translates the wedges
         # [num_wedges, 3]
         wedges_origin += translation_vector
 
         # Updates the wedges
-        wedges_ind = tf.expand_dims(wedges_ind, axis=1)
+        wedges_ind = np.expand_dims(wedges_ind, axis=1)
         solver_paths.wedges_origin.scatter_nd_update(wedges_ind, wedges_origin)
 
         # Trigger scene callback
@@ -258,7 +263,7 @@ class SceneObject(Object):
     def orientation(self, new_orient):
 
         # Real dtype
-        new_orient = tf.cast(new_orient, self._rdtype)
+        new_orient = np.asarray(new_orient, self._rdtype)
 
         # Build the transformtation corresponding to the new rotation
         new_rotation = angles_to_mitsuba_rotation(new_orient)
@@ -270,28 +275,30 @@ class SceneObject(Object):
         # Build the transform.
         # The object is first translated to the origin, then rotated, then
         # translated back to its current position
-        transform =  (  self._mi_transform_t.translate(self.position.numpy())
-                      @ new_rotation
-                      @ inv_cur_rotation
-                      @ self._mi_transform_t.translate(-self.position.numpy()) )
+        transform = (
+            self._mi_transform_t.translate(self.position.numpy())
+            @ new_rotation
+            @ inv_cur_rotation
+            @ self._mi_transform_t.translate(-self.position.numpy())
+        )
 
-        ## Update Mitsuba vertices
+        # Update Mitsuba vertices
 
         # Scene parameters
         scene_params = self._scene.mi_scene_params
         # [num_vertices*3]
-        vertices = scene_params[f'mesh-{self.name}.vertex_positions']
+        vertices = scene_params[f"mesh-{self.name}.vertex_positions"]
         # [num_vertices,3]
         vertices = dr.unravel(self._mi_point_t, vertices)
         # Apply the transform
         vertices = transform.transform_affine(vertices)
         # Cast to Mitsuba type to object the Mitsuba scene
-        fltn_vertices = tf.reshape(vertices, [-1])
-        fltn_vertices = tf.cast(fltn_vertices, tf.float32)
-        scene_params[f'mesh-{self.name}.vertex_positions'] = fltn_vertices
+        fltn_vertices = np.reshape(vertices, [-1])
+        fltn_vertices = np.asarray(fltn_vertices, np.float_)
+        scene_params[f"mesh-{self.name}.vertex_positions"] = fltn_vertices
         scene_params.update()
 
-        ## Update Sionna vertices
+        # Update Sionna vertices
 
         obj_id = self.object_id
         mi_shape = self._mi_shape
@@ -300,8 +307,9 @@ class SceneObject(Object):
         shape_ind = solver_paths.shape_indices[obj_id]
         prim_offset = solver_paths.prim_offsets[shape_ind]
 
-        face_indices3 = mi_shape.face_indices(dr.arange(mi.UInt32,
-                                                        mi_shape.face_count()))
+        face_indices3 = mi_shape.face_indices(
+            dr.arange(mi.UInt32, mi_shape.face_count())
+        )
         # Flatten. This is required for calling vertex_position
         # [n_prims*3]
         face_indices = dr.ravel(face_indices3)
@@ -310,17 +318,16 @@ class SceneObject(Object):
         vertex_coords = mi_shape.vertex_position(face_indices)
         # Cast to TensorFlow type
         # [n_prims*3, 3]
-        vertex_coords = mi_to_tf_tensor(vertex_coords, self._rdtype)
+        vertex_coords = mi_to_np_ndarray(vertex_coords, self._rdtype)
         # Unflatten
         # [n_prims, vertices per triangle : 3, 3]
-        vertex_coords = tf.reshape(vertex_coords, [mi_shape.face_count(), 3, 3])
+        vertex_coords = np.reshape(vertex_coords, [mi_shape.face_count(), 3, 3])
         # Update the tensor storing the primitive vertices
-        sl = tf.range(prim_offset, prim_offset + mi_shape.face_count(),
-                    dtype=tf.int32)
-        sl = tf.expand_dims(sl, axis=1)
+        sl = np.arange(prim_offset, prim_offset + mi_shape.face_count(), dtype=np.int_)
+        sl = np.expand_dims(sl, axis=1)
         solver_paths.primitives.scatter_nd_update(sl, vertex_coords)
 
-        ## Update Sionna normals
+        # Update Sionna normals
 
         # Get vertices coordinates
         # [n_prims, 3]
@@ -332,11 +339,11 @@ class SceneObject(Object):
         normals = transform.transform_affine(normals)
         # Cast to Tensorflow type
         # [n_prims, 3]
-        normals = mi_to_tf_tensor(normals, self._rdtype)
+        normals = mi_to_np_ndarray(normals, self._rdtype)
         # Update the tensor storing the primitive vertices
         solver_paths.normals.scatter_nd_update(sl, normals)
 
-        ## Update Sionna wedges
+        # Update Sionna wedges
 
         wedges_objects = solver_paths.wedges_objects
         wedges_origin = solver_paths.wedges_origin
@@ -345,17 +352,17 @@ class SceneObject(Object):
 
         # Indices of the wedges corresponding to this object
         # [num_wedges]
-        wedges_ind, _ = tf.unique(tf.where(wedges_objects == obj_id)[:,0])
+        wedges_ind, _ = np.unique(np.where(wedges_objects == obj_id)[:, 0])
 
         # Corresponding origins, e_hat, and normals
         # [num_wedges, 3]
-        wedges_origin = tf.gather(wedges_origin, wedges_ind, axis=0)
+        wedges_origin = np.take_along_axis(wedges_origin, wedges_ind, axis=0)
         # [num_wedges, 3]
-        wedges_e_hat = tf.gather(wedges_e_hat, wedges_ind, axis=0)
+        wedges_e_hat = np.take_along_axis(wedges_e_hat, wedges_ind, axis=0)
         # [num_wedges, 3]
-        wedges_normals = tf.gather(wedges_normals, wedges_ind, axis=0)
+        wedges_normals = np.take_along_axis(wedges_normals, wedges_ind, axis=0)
         # [num_wedges*2, 3]
-        wedges_normals = tf.reshape(wedges_normals, [-1, 3])
+        wedges_normals = np.reshape(wedges_normals, [-1, 3])
 
         # Cast to Mitsuba types
         # [num_wedges, 3]
@@ -368,27 +375,26 @@ class SceneObject(Object):
         # Rotate all quantities
         # [num_wedges, 3]
         wedges_origin = transform.transform_affine(wedges_origin)
-         # [num_wedges, 3]
+        # [num_wedges, 3]
         wedges_e_hat = transform.transform_affine(wedges_e_hat)
-         # [num_wedges*2, 3]
+        # [num_wedges*2, 3]
         wedges_normals = transform.transform_affine(wedges_normals)
 
         # Cast to Tensorflow type
         # [num_wedges, 3]
-        wedges_origin = mi_to_tf_tensor(wedges_origin, self._rdtype)
+        wedges_origin = mi_to_np_ndarray(wedges_origin, self._rdtype)
         # [num_wedges, 3]
-        wedges_e_hat = mi_to_tf_tensor(wedges_e_hat, self._rdtype)
+        wedges_e_hat = mi_to_np_ndarray(wedges_e_hat, self._rdtype)
         # [num_wedges*2, 3]
-        wedges_normals = mi_to_tf_tensor(wedges_normals, self._rdtype)
+        wedges_normals = mi_to_np_ndarray(wedges_normals, self._rdtype)
         # [num_wedges, 2, 3]
-        wedges_normals = tf.reshape(wedges_normals, [-1, 2, 3])
+        wedges_normals = np.reshape(wedges_normals, [-1, 2, 3])
 
         # Updates the wedges
-        wedges_ind = tf.expand_dims(wedges_ind, axis=1)
+        wedges_ind = np.expand_dims(wedges_ind, axis=1)
         solver_paths.wedges_origin.scatter_nd_update(wedges_ind, wedges_origin)
         solver_paths.wedges_e_hat.scatter_nd_update(wedges_ind, wedges_e_hat)
-        solver_paths.wedges_normals.scatter_nd_update(wedges_ind,
-                                                      wedges_normals)
+        solver_paths.wedges_normals.scatter_nd_update(wedges_ind, wedges_normals)
 
         self._orientation = new_orient
 
@@ -411,21 +417,23 @@ class SceneObject(Object):
         if isinstance(target, str):
             obj = self.scene.get(target)
             if not isinstance(obj, Object):
-                raise ValueError(f"No camera, device, or object named '{target}' found.")
+                raise ValueError(
+                    f"No camera, device, or object named '{target}' found."
+                )
             else:
                 target = obj.position
         elif isinstance(target, Object):
             target = target.position
         else:
-            target = tf.cast(target, dtype=self._rdtype)
-            if not target.shape[0]==3:
+            target = np.asarray(target, dtype=self._rdtype)
+            if not target.shape[0] == 3:
                 raise ValueError("`target` must be a three-element vector)")
 
         # Compute angles relative to LCS
         x = target - self.position
         x, _ = normalize(x)
         theta, phi = theta_phi_from_unit_vec(x)
-        alpha = phi # Rotation around z-axis
-        beta = theta-PI/2 # Rotation around y-axis
-        gamma = 0.0 # Rotation around x-axis
+        alpha = phi  # Rotation around z-axis
+        beta = theta - np.pi / 2  # Rotation around y-axis
+        gamma = 0.0  # Rotation around x-axis
         self.orientation = (alpha, beta, gamma)
